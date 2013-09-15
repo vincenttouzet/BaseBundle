47e7370f7c44d47ac7e49ea959227c939f88a881
Vincent Touzet <vincent.touzet@gmail.com>
2013-09-15 12:42:46 +0200

    Add Base code for service

---
+++ Service/Service.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace VinceT\BaseBundle\Service;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class Service implements ContainerAwareInterface
+{
+    protected $container = null;
+
+    /**
+     * __construct
+     *
+     * @param ContainerInterface $container A ContainerInterface instance
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->setContainer($container);
+    }
+
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface $container A ContainerInterface instance
+     *
+     * @return null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+}
